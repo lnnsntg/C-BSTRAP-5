a641d54a988b2809bf809ae801db111fe344734e
lenin <lnnsntg@gmail.com>
2021-09-01 02:21:56 +0200

    Añadi toasts componente

--- 05-formulario/app.js
+++ 05-formulario/app.js
@@ -1,10 +1,9 @@
 console.log("funcionando");
 
-
 const formulario = document.querySelector("#formulario");
 const btnEnviar = document.querySelector("#btnEnviar");
 const btnCargando = document.querySelector("#btnCargando");
-
+const toast = document.querySelector(".toast");
 
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,15 +12,14 @@ formulario.addEventListener("submit", (event) => {
   console.log("Clg Campo del Email: ", datos.get("email"));
   console.log("Clg Campo del Pass: ", datos.get("password"));
   console.log("Clg Campo del Checkbox: ", datos.get("checkbox"));
-  btnEnviar.classList.add("d-none")
-  btnCargando.classList.remove("d-none")
+  btnEnviar.classList.add("d-none");
+  btnCargando.classList.remove("d-none");
 
   setTimeout(() => {
-    btnCargando.classList.add("d-none")
-    btnEnviar.classList.remove("d-none")
-
-    
-  }, 3000);
-  formulario.reset()
+    btnCargando.classList.add("d-none");
+    btnEnviar.classList.remove("d-none");
+    const eventoToast = new bootstrap.Toast(toast);
+    eventoToast.show();
+  }, 1000);
+  formulario.reset();
 });
-
