835dd2a3d910ad815ae4de2b595fdbd55dfbf49c
lenin <lnnsntg@gmail.com>
2021-09-06 16:03:51 +0200

    Añadida carpeta 09-chat-bootstrap-firebase

--- 08-rango/js/javascript.js
+++ 08-rango/js/javascript.js
@@ -1,1 +1,11 @@
-console.log('hola');
+const precio = document.getElementById("precio");
+const vistas = document.getElementById("vistas");
+const inputRango = document.getElementById("customRange3");
+const arrayVisitas = ["10k", "50k", "100k", "500k", "1M"];
+console.log(inputRango.value);
+
+inputRango.addEventListener("input", () => {
+  precio.textContent = inputRango.value;
+  console.log(inputRango.value);
+  vistas.textContent = arrayVisitas[inputRango.value / 8 - 1];
+});
