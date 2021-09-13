0ee843c67c91ba5d2d88ca1835965b71bbee1796
lenin <lnnsntg@gmail.com>
2021-09-13 18:25:12 +0200

    Añadido app.js y cambio en el nombre de rama de master a a main

--- 09-chat-boostrap-firebase/js/app.js
+++ 09-chat-boostrap-firebase/js/app.js
@@ -1,1 +1,6 @@
-console.log("app");
+const nombreUsuario = document.getElementById("nombreUsuario");
+const btnIngreso = document.getElementById("btnIngreso");
+const btnCerrarSesion = document.getElementById("btnCerrarSesion");
+const contenidoWeb = document.getElementById("contenidoWeb");
+const formulario = document.getElementById("formulario");
+const texto = document.getElementById("texto");
