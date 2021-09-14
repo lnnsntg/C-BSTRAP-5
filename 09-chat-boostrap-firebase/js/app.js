250fc0b2153c67e3971de8aa25da3134bd25adc5
lenin <lnnsntg@gmail.com>
2021-09-14 04:16:43 +0200

    Añadida funciones auth y bibliotecas firebase

--- 09-chat-boostrap-firebase/js/app.js
+++ 09-chat-boostrap-firebase/js/app.js
@@ -4,3 +4,40 @@ const btnCerrarSesion = document.getElementById("btnCerrarSesion");
 const contenidoWeb = document.getElementById("contenidoWeb");
 const formulario = document.getElementById("formulario");
 const texto = document.getElementById("texto");
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    accionCerrarSesion();
+    nombreUsuario.innerHTML = user.displayName;
+  } else {
+    accionAcceder();
+    nombreUsuario.innerHTML = "CHAT-Firebase-Bootstrap";
+  }
+});
+
+const accionAcceder = () => {
+  console.log("Sin usuario registrado");
+  formulario.classList.add("d-none");
+  contenidoWeb.innerHTML = `<p class="lead mt-5 text-center">Debes iniciar sesión</p>`;
+
+  btnIngreso.addEventListener("click", async () => {
+    console.log("Quieres ingresar");
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+const accionCerrarSesion = () => {
+  console.log("usuario registrado");
+  formulario.classList.remove("d-none");
+  btnCerrarSesion.addEventListener("click", () => {
+    console.log("Quieres cerrar sesión");
+    firebase.auth().signOut();
+  });
+};
