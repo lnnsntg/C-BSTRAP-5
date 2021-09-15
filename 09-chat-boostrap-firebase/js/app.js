c4efa7fce505ece0b652c002f459aed702b11546
lenin <lnnsntg@gmail.com>
2021-09-15 04:58:47 +0200

    Añadidas funciones BD de firebase

--- 09-chat-boostrap-firebase/js/app.js
+++ 09-chat-boostrap-firebase/js/app.js
@@ -7,13 +7,69 @@ const texto = document.getElementById("texto");
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    contenidoWeb.innerHTML = "";
     accionCerrarSesion();
     nombreUsuario.innerHTML = user.displayName;
+    contenidoChat(user);
   } else {
     accionAcceder();
     nombreUsuario.innerHTML = "CHAT-Firebase-Bootstrap";
   }
 });
+
+const contenidoChat = (user) => {
+  // ---------------------------------
+  formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(texto.value);
+
+    if (!texto.value.trim()) {
+      console.log("texto vacio");
+      return;
+    }
+
+    firebase
+      .firestore()
+      .collection("chat")
+      .add({
+        texto: texto.value,
+        uid: user.uid,
+        fecha: Date.now(),
+      })
+      .then((resp) => {
+        console.log("Texto agregado a firestore");
+      });
+
+    texto.value = "";
+  });
+  // --------------------------------------
+
+  firebase
+    .firestore()
+    .collection("chat")
+    .orderBy("fecha")
+    .onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          console.log("chat: ", change.doc.data());
+          if (user.uid === change.doc.data().uid) {
+            contenidoWeb.innerHTML += `
+                <div class="text-end"><span class="badge bg-primary">${
+                  change.doc.data().texto
+                }</span></div>
+                `;
+          } else {
+            contenidoWeb.innerHTML += `
+                <div class="text-start"><span class="badge bg-secondary">${
+                  change.doc.data().texto
+                }</span></div>
+                `;
+          }
+          contenidoWeb.scrollTo = contenidoWeb.scrollHeight;
+        }
+      });
+    });
+};
 
 const accionAcceder = () => {
   console.log("Sin usuario registrado");
